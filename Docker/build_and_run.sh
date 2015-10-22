5e7b0eed14e79808c6480cdd2b618c49da10e1b0
Dmytro Rashko <dimetron@me.com>
2015-10-22 22:46:48 +0200

    docker file fixes

--- Docker/build_and_run.sh
+++ Docker/build_and_run.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+
+#starting mongodb
+sudo docker kill mongo
+sudo docker rm   mongo
+sudo docker rmi  mongo
+sudo docker run -d --name=mongo--restart=always -h mongodb mongo
+
+#starting open-tracker
 sudo docker kill open-tracker
 sudo docker rm   open-tracker
 sudo docker rmi  open-tracker
 
 sudo docker build  --no-cache -t open-tracker .
-sudo docker run -d --name=open-tracker --restart=always -p 8080:8080 -p 9999:9999 open-tracker
+
+sudo docker run -d --name=open-tracker --restart=always --add-host mongo:$MONGOIP -p 8080:8080 -p 9999:9999 open-tracker
+
+
+sudo docker images
