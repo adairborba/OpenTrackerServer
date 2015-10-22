6ece26faa238e87104ce881deded3da631c52347
Dmytro Rashko <dimetron@me.com>
2015-10-22 22:27:37 +0200

    docker start script changed

--- Docker/build_and_run.sh
+++ Docker/build_and_run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+sudo docker kill open-tracker
+sudo docker rm   open-tracker
+sudo docker rmi  open-tracker
+
 sudo docker build  --no-cache open-tracker-server -t open-tracker .
 sudo docker run -d --name=open-tracker --restart=always -p 8080:8080 -p 9999:9999 open-tracker
