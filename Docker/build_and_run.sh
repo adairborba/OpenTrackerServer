36dbe84a68017887e8caf801480f4fd66abb186c
Dmytro Rashko <dimetron@me.com>
2015-10-22 22:47:33 +0200

    docker file fixes

--- Docker/build_and_run.sh
+++ Docker/build_and_run.sh
@@ -13,8 +13,7 @@ sudo docker rm   open-tracker
 sudo docker rmi  open-tracker
 
 sudo docker build  --no-cache -t open-tracker .
-
 sudo docker run -d --name=open-tracker --restart=always --add-host mongo:$MONGOIP -p 8080:8080 -p 9999:9999 open-tracker
 
-
 sudo docker images
+sudo docker ps -a
