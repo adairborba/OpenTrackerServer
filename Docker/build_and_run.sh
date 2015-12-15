1ff499da2934d0cb9ad3bf6d48ddff9496d2c6fb
Dmytro Rashko <dimetron@me.com>
2015-12-15 23:46:30 +0100

    Updated versions Scala/Activator/Sbt

--- Docker/build_and_run.sh
+++ Docker/build_and_run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#cleanup docker
+docker rm -v $(docker ps -a -q -f status=exited)
+docker rmi $(docker images -f "dangling=true" -q)
+
 #starting mosquito server
 docker kill mqtt
 docker rm   mqtt
@@ -7,10 +11,11 @@ docker rmi  toke/mosquitto
 docker run -d --name=mqtt --restart=always -tip 1883:1883 -p 9001:9001 toke/mosquitto
 
 #starting mongodb
+mkdir -p /opt/data/db
 docker kill mongo
 docker rm   mongo
 docker rmi  mongo
-docker run -d --name=mongo --restart=always  mongo
+docker run -d --name=mongo --restart=always -v /opt/data/db:/data/db  mongo
 
 #starting open-tracker
 chmod +x ./build_open_tracker.sh
